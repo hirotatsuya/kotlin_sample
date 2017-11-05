57ce8d5f457775da623348eeb25b253f1f19b49a
Tatsuya Hirota <h1208tatsuya@gmail.com>
2017-11-05 14:47:22 +0900

    update mysqlのバージョンの修正

--- src/main/kotlin/com/example/kotlin_sample/model/HelloModel.kt
+++ src/main/kotlin/com/example/kotlin_sample/model/HelloModel.kt
@@ -1,7 +1,6 @@
 package com.example.kotlin_sample.model
 
 import com.fasterxml.jackson.annotation.JsonCreator
-import kotliquery.Row
 
 class HelloModel @JsonCreator constructor (
         var text: Int,
