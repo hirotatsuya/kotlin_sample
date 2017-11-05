3bc652e159c01372acf0cc2ff1349c60e0463eee
Tatsuya Hirota <h1208tatsuya@gmail.com>
2017-11-05 16:00:57 +0900

    update get cook_list

---
+++ src/main/kotlin/com/example/kotlin_sample/repository/CookListRepository.kt
@@ -0,0 +1,44 @@
+package com.example.kotlin_sample.repository
+
+import com.example.kotlin_sample.BaseRepository
+import com.example.kotlin_sample.model.CookList
+import kotliquery.Session
+import kotliquery.queryOf
+import kotliquery.Row
+import org.springframework.stereotype.Repository
+
+@Repository
+class CookListRepository {
+
+    private val selectAll : String = "select * from cook_list";
+    private val selectCookListById: String = "select * from cook_list where id = ?";
+
+    val toCookList: (Row) -> CookList = { row ->
+        CookList(
+            row.int(columnLabel = "id"),
+            row.string(columnLabel = "name"),
+            row.int(columnLabel = "recipe_id"),
+            row.int(columnLabel = "setup_id"),
+            row.string(columnLabel = "img_path")
+        )
+    }
+
+    fun findAll() : MutableList<CookList> {
+        val returnedList : MutableList<CookList> = mutableListOf();
+        BaseRepository.getSession().use<Session, Unit> {
+            val selectCookList = queryOf(selectAll).map(toCookList).asList;
+            returnedList.addAll(it.run(selectCookList));
+        }
+        return returnedList;
+    }
+
+    fun getCookListById(id: Int): MutableList<CookList> {
+        val returnedList : MutableList<CookList> = mutableListOf();
+        BaseRepository.getSession().use<Session, Unit> {
+            val selectCookList = queryOf(selectCookListById, id).map(toCookList).asList;
+            returnedList.addAll(it.run(selectCookList));
+        }
+        return returnedList;
+    }
+}
+
